bf9c1e376d80bcd5924258ab780094e73f49a232
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2022-02-07 14:51:38 +0100

    encahnced app

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title("Hello World!")
+option = st.selectbox(
+     'How would you like to be contacted?',
+     ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', option)
